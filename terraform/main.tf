07eb481e4d26a16c1a41293f39637b002030755e
devops4sure <devops4sure@gmail.com>
2025-12-22 19:32:22 +0530

    Fix OIDC by referencing existing GitHub provider and finalize ECR IAM role

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,18 +11,10 @@ resource "aws_ecr_repository" "app" {
 }
 
 # -----------------------------
-# GitHub OIDC Provider
+# GitHub OIDC Provider (EXISTING)
 # -----------------------------
-resource "aws_iam_openid_connect_provider" "github" {
+data "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
-
-  client_id_list = [
-    "sts.amazonaws.com"
-  ]
-
-  thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1"
-  ]
 }
 
 # -----------------------------
@@ -37,12 +29,15 @@ resource "aws_iam_role" "github_actions" {
       {
         Effect = "Allow"
         Principal = {
-          Federated = aws_iam_openid_connect_provider.github.arn
+          Federated = data.aws_iam_openid_connect_provider.github.arn
         }
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
           StringLike = {
             "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:ref:refs/heads/main"
+          }
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
         }
       }
